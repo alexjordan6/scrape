9039c70b2b9bf52bb6b702e9ad4df11a6b106260
alex <alexjordan6@gmail.com>
2017-10-14 23:39:20 -0500

    removing quickscrape

--- quickScrape.ps1
+++
@@ -1,9 +0,0 @@
-. .\functions.ps1
-
-$url = "https://madison.craigslist.org/d/atvs%2C-utvs%2C-snowmobiles/search/sna"
-$webResp = getWebResponse -url $url
-
-$pricedResults = getPricedResults -links $webResp.links
-
-$pricedResults
-
