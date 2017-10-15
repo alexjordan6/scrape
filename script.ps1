6db80032d0153379f303d00ade659a4a999896c6
alex <alexjordan6@gmail.com>
2017-10-14 22:03:12 -0500

    adding use basic parsing,?

--- script.ps1
+++ script.ps1
@@ -1,9 +1,9 @@
 $url = "https://madison.craigslist.org/d/atvs%2C-utvs%2C-snowmobiles/search/sna"
 
 try {
-    $results = Invoke-WebRequest $url
-    $results
+    $results = Invoke-WebRequest $url -UseBasicParsing
+    
 }
 catch {
-
+    $_
 }
